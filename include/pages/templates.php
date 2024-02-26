ba9d880a5f00898d4fb21cc666b050a659a2e876
Daan5556 <egdaan@gmail.com>
2024-02-26 11:02:54 +0100

    Sponsors

---
+++ include/pages/templates.php
@@ -0,0 +1,99 @@
+<div class="standard-box">
+    <h2>Lorem Ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque libero numquam soluta ullam laboriosam,
+        voluptate nam ducimus. Totam fugiat, consequatur earum dolore nulla, natus magni ut commodi eligendi quidem
+        omnis
+        eos excepturi a! Minima qui beatae ex non, illo tempore in reiciendis. Pariatur impedit harum voluptas
+        voluptatem
+        temporibus recusandae quisquam, non neque in iste tenetur ipsa exercitationem corporis saepe id reiciendis nobis
+        porro praesentium qui optio aliquid tempora deserunt fugit. Sequi veniam animi mollitia. Quis voluptate
+        possimus,
+        reprehenderit numquam modi atque, soluta dolor illo, dolores corrupti architecto iste illum ipsam ad
+        necessitatibus
+        voluptates! Fuga temporibus tenetur alias dicta in natus!
+    </p>
+</div>
+
+<div class="standard-box">
+    <h2>Lorem Ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque libero numquam soluta ullam laboriosam,
+        voluptate nam ducimus. Totam fugiat, consequatur earum dolore nulla, natus magni ut commodi eligendi quidem
+        omnis
+        eos excepturi a! Minima qui beatae ex non, illo tempore in reiciendis. Pariatur impedit harum voluptas
+        voluptatem
+        temporibus recusandae quisquam, non neque in iste tenetur ipsa exercitationem corporis saepe id reiciendis nobis
+        porro praesentium qui optio aliquid tempora deserunt fugit. Sequi veniam animi mollitia. Quis voluptate
+        possimus,
+        reprehenderit numquam modi atque, soluta dolor illo, dolores corrupti architecto iste illum ipsam ad
+        necessitatibus
+        voluptates! Fuga temporibus tenetur alias dicta in natus!
+    </p>
+</div>
+<div class="standard-box">
+    <h2>Lorem Ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque libero numquam soluta ullam laboriosam,
+        voluptate nam ducimus. Totam fugiat, consequatur earum dolore nulla, natus magni ut commodi eligendi quidem
+        omnis
+        eos excepturi a! Minima qui beatae ex non, illo tempore in reiciendis. Pariatur impedit harum voluptas
+        voluptatem
+        temporibus recusandae quisquam, non neque in iste tenetur ipsa exercitationem corporis saepe id reiciendis nobis
+        porro praesentium qui optio aliquid tempora deserunt fugit. Sequi veniam animi mollitia. Quis voluptate
+        possimus,
+        reprehenderit numquam modi atque, soluta dolor illo, dolores corrupti architecto iste illum ipsam ad
+        necessitatibus
+        voluptates! Fuga temporibus tenetur alias dicta in natus!
+    </p>
+</div>
+
+<div class="standard-box">
+    <h2>Lorem Ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque libero numquam soluta ullam laboriosam,
+        voluptate nam ducimus. Totam fugiat, consequatur earum dolore nulla, natus magni ut commodi eligendi quidem
+        omnis
+        eos excepturi a! Minima qui beatae ex non, illo tempore in reiciendis. Pariatur impedit harum voluptas
+        voluptatem
+        temporibus recusandae quisquam, non neque in iste tenetur ipsa exercitationem corporis saepe id reiciendis nobis
+        porro praesentium qui optio aliquid tempora deserunt fugit. Sequi veniam animi mollitia. Quis voluptate
+        possimus,
+        reprehenderit numquam modi atque, soluta dolor illo, dolores corrupti architecto iste illum ipsam ad
+        necessitatibus
+        voluptates! Fuga temporibus tenetur alias dicta in natus!
+    </p>
+</div>
+<div class="standard-box">
+    <h2>Lorem Ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque libero numquam soluta ullam laboriosam,
+        voluptate nam ducimus. Totam fugiat, consequatur earum dolore nulla, natus magni ut commodi eligendi quidem
+        omnis
+        eos excepturi a! Minima qui beatae ex non, illo tempore in reiciendis. Pariatur impedit harum voluptas
+        voluptatem
+        temporibus recusandae quisquam, non neque in iste tenetur ipsa exercitationem corporis saepe id reiciendis nobis
+        porro praesentium qui optio aliquid tempora deserunt fugit. Sequi veniam animi mollitia. Quis voluptate
+        possimus,
+        reprehenderit numquam modi atque, soluta dolor illo, dolores corrupti architecto iste illum ipsam ad
+        necessitatibus
+        voluptates! Fuga temporibus tenetur alias dicta in natus!
+    </p>
+</div>
+
+<div class="standard-box">
+    <h2>Lorem Ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque libero numquam soluta ullam laboriosam,
+        voluptate nam ducimus. Totam fugiat, consequatur earum dolore nulla, natus magni ut commodi eligendi quidem
+        omnis
+        eos excepturi a! Minima qui beatae ex non, illo tempore in reiciendis. Pariatur impedit harum voluptas
+        voluptatem
+        temporibus recusandae quisquam, non neque in iste tenetur ipsa exercitationem corporis saepe id reiciendis nobis
+        porro praesentium qui optio aliquid tempora deserunt fugit. Sequi veniam animi mollitia. Quis voluptate
+        possimus,
+        reprehenderit numquam modi atque, soluta dolor illo, dolores corrupti architecto iste illum ipsam ad
+        necessitatibus
+        voluptates! Fuga temporibus tenetur alias dicta in natus!
+    </p>
+</div>
