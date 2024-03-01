26cb71ffbb911d85b78e51191c0137af72e1a1e0
Daan5556 <egdaan@gmail.com>
2024-03-01 11:45:40 +0100

    account_pages

---
+++ include/pages/account/accountOverview.php
@@ -0,0 +1,33 @@
+<div class="account-overview">
+    <div class="account-top">
+        <span class="profile-pic">D</span>
+        Username
+        <button>Log uit</button>
+    </div>
+    <div class="tickets">
+        <h3>Mijn Tickets</h3>
+        <table>
+            <tr>
+                <th>Evenement</th>
+                <th>Datum</th>
+                <th>Hoeveelheid</th>
+            </tr>
+            <tr>
+                <td>Bollen Racing</td>
+                <td>23/22/</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Bollen Racing</td>
+                <td>23/22/</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Bollen Racing</td>
+                <td>23/22/</td>
+                <td>3</td>
+            </tr>
+
+        </table>
+    </div>
+</div>
---
+++ include/pages/account/login.php
@@ -0,0 +1,15 @@
+<div class="login">
+    <h2>Login</h2>
+    <form action="">
+        <div class="login-row">
+            <label for="username">Gebruikersnaam</label>
+            <input type="text" name="username">
+        </div>
+        <div class="login-row">
+            <label for="password">Wachtwoord</label>
+            <input type="password">
+        </div>
+        <input type="submit" value="Versturen">
+    </form>
+    <a href="?accountPage=createAccount">Account aanmaken</a>
+</div>
