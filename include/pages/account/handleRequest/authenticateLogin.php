7cb2e574e2e75ea2e74f6bc26a1679b79d1e8b91
Daan5556 <egdaan@gmail.com>
2024-03-04 12:47:16 +0100

    authentication Encrypt

--- include/pages/account/handleRequest/authenticateLogin.php
+++ include/pages/account/handleRequest/authenticateLogin.php
@@ -20,16 +20,37 @@ function validateLogin($username, $password, $conn) {
   return false;
 }
 
+function validateLoginHashed($username, $password, $conn) {
+  $hashedPassword = getHashedPassword($username, $conn);
+
+  if ($hashedPassword != null) {
+    echo $password . "<br>";
+    echo $hashedPassword . "<br>";
+    return password_verify($password, $hashedPassword);
+  }
+  return false;
+}
+
+function getHashedPassword($username, $conn) {
+  $query = "SELECT user_password FROM user_information WHERE user_name = '$username'";
+  $result = $conn->query($query);
+
+  if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    return $row["user_password"];
+  }
+  return null;
+}
+
 session_start();
 
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-if (validateLogin($username, $password, $conn)) {
+if (validateLoginHashed($username, $password, $conn)) {
   $_SESSION["username"] = $username;
 } else {
   echo "login failed";
-  $_SESSION["login-error"] = "Gebruikersnaam & Wachtwoord komen niet overheen";
 }
 
 header("location:/main.php?page=" . $page);
