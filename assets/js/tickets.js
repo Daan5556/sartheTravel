d01dac2190cda9003b4e971b3afb9c46c20af943
mackie222 <517039@vistacollege.nl>
2024-03-06 13:04:39 +0100

    errorhandling in tickets specified css

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -4,19 +4,36 @@ let vip = document.getElementById("vip")
 let button = document.getElementById("checkout-button")
 let totalPrice = document.getElementById("totalPrice")
 function calculatePrice(){
-    console.log('func')
-    console.log(kids.value)
-    console.log(volwassenen.value)
-    console.log(vip.value)  
+
 if (kids.value > 99) { 
     alert("maximale aantal tickets bereikt")
+    if (error){
+        alert()
+        return
+    }
 }  
 if (vip.value > 5) { 
     alert("Je mag niet meer dan 5 vip tickets b etellen")
+    if (error){
+        alert()
+        return
+    }
 }  
 if (volwassenen.value > 99) { 
     alert("maximale aantal tickets bereikt")
-}          
+    if (error){
+        alert()
+        return
+    }
+}
+if (kids.value < 0 || vip.value < 0 || volwassenen.value < 0){
+    alert("minimale aantal tickets")
+    if (error){
+        alert()
+        return
+    }
+}
+
 let total = kids.value * 10 + volwassenen.value * 25 + vip.value * 50
 if (isNaN(total)) {
     alert("Voer een getal in");
--- include/pages/tickets.php
+++ include/pages/tickets.php
@@ -1,19 +1,45 @@
-    <div class="containertickets">
-        <h2>Race Tickets</h2>
-       <img  id="track-image" src="../../assets/images/racetrack.png" alt="Race Map">
+<div class="containertickets">
+  <h2>Race Tickets</h2>
+  <img
+    id="track-image"
+    src="../../assets/images/racetrack.png"
+    alt="Race Map"
+  />
+  <div class="headContainer">
     <div class="flexTickets">
       <label for="kids">kids €10,-</label>
-      <input  id="kids"type="number" name="kids"> 
+      <input id="kids" type="number" name="kids" />
       <label for="volwassenen">volwassenen €25,-</label>
-      <input id="volwassenen" type="number" name="kids" >
+      <input id="volwassenen" type="number" name="kids" />
       <label for="vip">VIP €50,-</label>
-      <input id="vip" type="number" name="kids">
-      <button id="checkout-button">Checkout</button>
-</div>
-    </div>
+      <input id="vip" type="number" name="kids" />
+      <button role="button" id="checkout-button">Checkout</button>
+      </div>
+      <div class="info-tickets">
+        
+        <ol>
+          Kids tickets: 0 t/m 17 jaar<br />
+          Kinderwagens achterlaten op aangewezen plaats aub<br />
 
-    <div class="Checkout-box">
-        <h2>Checkout</h2>
-        <p>You have selected <span id="selected-ticket"></span> ticket(s). Total Price: €<span id="totalPrice"></span></p>
-    </div>
-    <script src="assets/js/tickets.js"></script>>
+          <br />18 t/m 65+ jaar<br />
+          nog geen 21 jaar, ID laten zien<br />
+
+          <br />Speciale vip plaatsen gereserveerd en toegang tot de VIP tent<br />
+          VIP Lounge<br />
+          Tour
+        </ol>
+      </div>
+</div>
+</div>
+<div class="Checkout-box">
+  <h2>Checkout</h2>
+  
+  <p>
+    You have selected <span id="selected-ticket"></span> ticket(s). Total Price:
+    €<span id="totalPrice"></span>
+  </p>
+</div>
+
+
+<script src="assets/js/tickets.js"></script>
+>
