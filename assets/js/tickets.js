24a5842e01ae411ab231dff03e43e11af4103135
Daan5556 <egdaan@gmail.com>
2024-03-07 17:10:47 +0100

    Error handling tickets (negative)

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -3,26 +3,32 @@ let volwassenen = document.getElementById("volwassenen")
 let vip = document.getElementById("vip")
 let button = document.getElementById("checkout-button")
 let totalPrice = document.getElementById("totalPrice")
-function calculatePrice(){
-    console.log('func')
-    console.log(kids.value)
-    console.log(volwassenen.value)
-    console.log(vip.value)  
-if (kids.value > 99) { 
-    alert("maximale aantal tickets bereikt")
-}  
-if (vip.value > 5) { 
-    alert("Je mag niet meer dan 5 vip tickets b etellen")
-}  
-if (volwassenen.value > 99) { 
-    alert("maximale aantal tickets bereikt")
-}          
-let total = kids.value * 10 + volwassenen.value * 25 + vip.value * 50
-if (isNaN(total)) {
+
+function calculatePrice() {
+  if (isNaN(kids.value) || isNaN(volwassenen.value) || isNaN(vip.value)) {
     alert("Voer een getal in");
-} else {
-   totalPrice.innerHTML=total
-}
+    return
+  }
+  if (kids.value > 99) {
+    alert("maximale aantal tickets bereikt")
+    return
+  }
+  if (volwassenen.value > 99) {
+    alert("maximale aantal tickets bereikt")
+    return
+  }
+  if (vip.value > 5) {
+    alert("Je mag niet meer dan 5 vip tickets bestellen")
+    return
+  }
+  if (kids.value < 0 || volwassenen.value < 0 || vip.value < 0) {
+    alert("Kan niet minder dan 0 tickets bestellen")
+    return
+  }
+
+  let total = kids.value * 10 + volwassenen.value * 25 + vip.value * 50
+
+  totalPrice.innerHTML = total
 }
 
 button.addEventListener("click", calculatePrice)
