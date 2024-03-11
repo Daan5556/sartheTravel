b12465f0b1b26f2b40d2ff3fcf10562e7e259f84
DJSmurfie <jarno.busing@gmail.com>
2024-03-11 13:15:38 +0100

    Contact verbeterd

--- include/pages/contact.php
+++ include/pages/contact.php
@@ -1,84 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-    <div class="contact">
-        <h2>Contact Us</h2>
-            <div class="vNaamContact">
-                <label for="voornaam">Voornaam:</label>
-                <input type="text" id="voornaam" name="voornaam" required>
-            </div>
-            <div class="aNaamContact">
-                <label for="achternaam">Achternaam:</label>
-                <input type="text" id="achternaam" name="achternaam" required>
-            </div>
-            <div class="emailContact">
-                <label for="email">E-mailadres:</label>
-                <input type="text" id="email" name="email" required>
-            </div>
-            <div class="tnContact">
-                <label for="tnummer">Telefoonnummer:</label>
-                <input type="text" id="tnummer" name="tnummer" required>
-            </div>
+<div class="contact">
+  <div class="gwenContainer">
+    <a
+      href="https://be.linkedin.com/in/dieter-bollen-967a9818a"
+      target="_blank"
+    >
+      <img
+        class="gwenImage"
+        src="assets\images\bollen_strijder.jpg"
+        alt="Gwen Bollen"
+      />
+    </a>
+    <div class="overlay">
+      <div class="gwenInfo">Gwen Bollen</div>
     </div>
-    <div class="GwenBollen">
-              <img src="" alt="Gwen Bollen">
-            </div>
-    <div class="gwenText">
-    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Laboriosam neque ullam fugiat, debitis odit laborum.</h3>
-    </div>
-  </main>
-
-
-  <div class="nav-menu">
-    <ul>
-      <a href="#">
-        <li class="nav-item">
-          <span class="material-symbols-outlined">
-            info
-          </span>
-          <h3>Info</h3>
-        </li>
-      </a>
-      <a href="#">
-        <li class="nav-item"><span class="material-symbols-outlined">
-            payments
-          </span>
-          <h3>Tickets</h3>
-        </li>
-      </a>
-      <a href="#">
-        <li class="nav-item"><span class="material-symbols-outlined">
-          </span>
-          <h3>Blog</h3>
-        </li>
-      </a>
-      <a href="#">
-        <li class="nav-item">
-          <span class="material-symbols-outlined">
-            mail
-          </span>
-          <h3>Contact</h3>
-        </li>
-      </a>
-    </ul>
   </div>
-
-  <div class="settings" id="settings">  
-    <div class="lang-select">
-      <img src="assets/images/dutch-flag.svg" alt="">
-      <img src="assets/images/english-flag.svg" alt="">
+  <h2 class="ContactUs">Contact Us</h2>
+  <div class="formContainer">
+    <div class="vNaamContact">
+      <label for="voornaam">Voornaam:</label>
+      <input
+        type="text"
+        id="voornaam"
+        name="voornaam"
+        placeholder="Vul hier uw voornaam in..."
+        maxlength="30"
+        required
+      />
     </div>
-    <div class="dark-mode">
-      <span class="material-symbols-outlined dark active">
-        light_mode
-      </span>
-      <span class="material-symbols-outlined">
-        dark_mode
-      </span>
+    <div class="aNaamContact">
+      <label for="achternaam">Achternaam:</label>
+      <input
+        type="text"
+        id="achternaam"
+        name="achternaam"
+        placeholder="Vul hier uw achternaam in..."
+        maxlength="30"
+        required
+      />
     </div>
-    <span class="material-symbols-outlined" id="settingsCloseButton">
-      close
-    </span>
   </div>
-</body>
-</html>
+  <div class="commentContact">
+    <label for="comment">Comment:</label>
+    <textarea placeholder="Enter your comment..." spellcheck="false"></textarea>
+  </div>
+</div>
