cb15800abf6eb95e91f888f745e99495b456e87f
Sephtonlol <132364058+Sephtonlol@users.noreply.github.com>
2024-03-11 12:29:54 +0100

    Update home.php

--- include/pages/home.php
+++ include/pages/home.php
@@ -1,3 +1,19 @@
-<div class="container">
-    <img src="./assets/images/racetrackBackground.jpg" alt="racetrack">
+<div class="container-home">
+
 </div>      
+
+<style>
+.container-home {
+    overflow: hidden;  
+    position: relative;
+  z-index: -1;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  min-height: 600px;
+  max-width: 10000px;
+  min-width: none;
+  background-color: blue;
+  background-image: url("./assets/images/racetrackBackground.jpg");
+}
+</style>
