fb445347f4a2d4a5a601b2591198ee321b0f6c58
Sephtonlol <132364058+Sephtonlol@users.noreply.github.com>
2024-03-12 12:28:29 +0100

    link to pages

--- include/pages/home.php
+++ include/pages/home.php
@@ -1,59 +1,75 @@
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Bungee+Outline&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Bungee+Outline&display=swap"
+  rel="stylesheet"
+/>
 
 <div class="container-home">
   <div>
     <span class="text">GET READY</span>
   </div>
   <div class="widgetsContainer">
-    <div class="widgets">
+    <a href="?page=tickets" class="widgets">
       <div class="textWidgets">
-        <span>T<br/>‎ I<br/>C<br/>K<br/>E<br/>T<br/>S</span>
+        <span>T<br />‎ I<br />C<br />K<br />E<br />T<br />S</span>
       </div>
       <div class="tickets-widget">
         <span class="widgetText">Volgende Race: 12 Mei</span>
-        <br/>
+        <br />
         <span class="widgetText">Uw Tickets: 12</span>
         <span class="widgetText">VIP Tickets: 3</span>
         <span class="widgetText">Volwassene Tickets: 4</span>
         <span class="widgetText">Kinder Tickets: 5</span>
-        <br/>
+        <br />
         <span class="widgetText">Beschikbare Plaatsen: 12</span>
       </div>
-    </div>
-    <div class="widgets">
+    </a>
+    <a href="?page=blog" class="widgets">
       <div class="textWidgets">
-        <span>B<br/>L<br/>O<br/>G</span>
+        <span>B<br />L<br />O<br />G</span>
       </div>
       <div class="blog">
         <div class="post">
-          <img class="imageBlog" src="./assets/images/blogfoto.jpg" alt="blog"/>
+          <img
+            class="imageBlog"
+            src="./assets/images/blogfoto.jpg"
+            alt="blog"
+          />
           <div class="postText">
             <h3>Wortels in racebaan?</h3>
-            <span class="textPost">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            <span class="textPost"
+              >Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Aspernatur voluptatem temporibus reiciendis repellendus eos
               eligendi doloremque magni a id inventore ut unde officiis,
               explicabo tempore earum velit doloribus cumque quibusdam laborum
               suscipit, nisi quod animi blanditiis. Molestias omnis cum corporis
               fugiat laudantium aut, asperiores explicabo, assumenda, dolores
-              repellendus fugit facilis.</span>
+              repellendus fugit facilis.</span
+            >
           </div>
         </div>
       </div>
-    </div>
-    <div class="widgets">
+    </a>
+    <a href="?page=contact" class="widgets">
       <div class="textWidgets">
-        <span>C<br/>O<br/>N<br/>T<br/>A<br/>C<br/>T</span>
+        <span>C<br />O<br />N<br />T<br />A<br />C<br />T</span>
       </div>
       <div class="contact">
         <div class="contactText">
-          <img class="imageContact" src="./assets/images/DieterBollen.png" alt="blog"/>
+          <img
+            class="imageContact"
+            src="./assets/images/DieterBollen.png"
+            alt="blog"
+          />
           <h3>Gwen Bollen</h3>
-          <span class="contactIcons"><span class="material-symbols-outlined"> mail </span><span
-              class="material-symbols-outlined"> call </span><span class="material-symbols-outlined"> fax </span></span>
+          <span class="contactIcons"
+            ><span class="material-symbols-outlined"> mail </span
+            ><span class="material-symbols-outlined"> call </span
+            ><span class="material-symbols-outlined"> fax </span></span
+          >
         </div>
       </div>
-    </div>
+    </a>
   </div>
 </div>
