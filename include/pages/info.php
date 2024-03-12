8e003a3d8dbb699f922c629b7a2e92f4efb9a7d9
mackie222 <517039@vistacollege.nl>
2024-03-12 12:18:39 +0100

    infopage

---
+++ include/pages/info.php
@@ -0,0 +1,24 @@
+<div class="container-info">
+    <div class="history">  
+        <img id="geschiedenis-foto" src="assets\images\Le-Mans-Historie-169FullWidth-a5094ebb-755804.avif" alt="auto">
+        <h2>History of the Circuit</h2>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem quas fuga explicabo vel nisi, eum illum accusamus laboriosam nihil est officia labore sed accusantium facilis ab molestiae ducimus repudiandae ut.
+            Unde maxime numquam, pariatur corporis nostrum reiciendis accusantium mollitia, fugiat doloribus nulla et natus nisi aperiam quae quaerat quam animi quas officiis dolorem optio repellendus modi vitae ut! Velit, modi!
+            Porro esse ab pariatur rem incidunt delectus, rerum fugit deleniti maxime vel voluptate voluptatibus culpa, id eos omnis earum voluptas, ratione doloremque optio? Hic consequatur, placeat maxime quaerat voluptatem aut!
+            In distinctio, ut, fugiat corrupti ipsam est voluptatum rerum, perspiciatis laboriosam commodi nemo? Ducimus temporibus tempore id cupiditate itaque omnis nihil enim consequuntur, perspiciatis voluptas quisquam eum tenetur provident veniam. 
+            <a id="linkje" href="https://en.wikipedia.org/wiki/Circuit_de_la_Sarthe" style="color: blue;">Klik hier om meer te lezen...</a>
+        </p>
+    </div>
+    <div class="history2">
+        <img id="geschiedenis-foto" src="assets\images\Le-Mans-Historie-169FullWidth-a5094ebb-755804.avif" alt="auto">  
+        <h2>Banaan</h2>
+        <p>  
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem quas fuga explicabo vel nisi, eum illum accusamus laboriosam nihil est officia labore sed accusantium facilis ab molestiae ducimus repudiandae ut.
+            Unde maxime numquam, pariatur corporis nostrum reiciendis accusantium mollitia, fugiat doloribus nulla et natus nisi aperiam quae quaerat quam animi quas officiis dolorem optio repellendus modi vitae ut! Velit, modi!
+            Porro esse ab pariatur rem incidunt delectus, rerum fugit deleniti maxime vel voluptate voluptatibus culpa, id eos omnis earum voluptas, ratione doloremque optio? Hic consequatur, placeat maxime quaerat voluptatem aut!
+            In distinctio, ut, fugiat corrupti ipsam est voluptatum rerum, perspiciatis laboriosam commodi nemo? Ducimus temporibus tempore id cupiditate itaque omnis nihil enim consequuntur, perspiciatis voluptas quisquam eum tenetur provident veniam.
+            <a id="linkje" href="https://en.wikipedia.org/wiki/Circuit_de_la_Sarthe" style="color: blue;">Klik hier om meer te lezen...</a>
+        </p>
+    </div>
+</div>
